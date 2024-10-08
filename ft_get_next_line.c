f4897bcf05370b538cb644526361ac4ad4269990
Javier <jaferco19@gmail.com>
2024-10-08 20:17:32 +0200

    some memory leaks solved

--- ft_get_next_line.c
+++ ft_get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_get_next_line.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaferna2 <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jaferna2 <jaferna2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 11:12:40 by jaferna2          #+#    #+#             */
-/*   Updated: 2024/10/04 12:15:18 by jaferna2         ###   ########.fr       */
+/*   Updated: 2024/10/08 20:15:07 by jaferna2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,19 @@ static void	freedom(void **ptr)
 
 static	char	*prepare_line_read(char *buffer, char *rest)
 {
-	char	*temp;
 	char	*line_read;
 
-	temp = NULL;
-	line_read = ft_strdup(buffer);
-	if (!line_read)
-		return (NULL);
 	if (rest)
 	{
-		temp = ft_strjoin(rest, line_read);
-		if (!temp)
+		line_read = ft_strjoin(rest, buffer);
+		if (!line_read)
 		{
-			freedom((void **)&line_read);
+			freedom((void **)&buffer);
 			return (NULL);
 		}
-		line_read = ft_strdup(temp);
-		freedom((void **)&temp);
 	}
+	else
+		line_read = ft_strdup(buffer);
 	return (line_read);
 }
 
@@ -59,12 +54,12 @@ char	*ft_get_next_line(int fd)
 	while (1)
 	{
 		bytes_read = read (fd, buffer, BUFFER_SIZE);
-		if (bytes_read == -1) // ERROR DE LECTURA // 
+		if (bytes_read == -1)
 		{
 			freedom((void **)&buffer);
 			return (NULL);
 		}
-		if (bytes_read == 0 && !line_read) // FIN DE LECTURA Y NO TENGO LINE_READ
+		if (bytes_read == 0 && !line_read)
 		{
 			freedom((void **)&buffer);
 			line_read = ft_strdup(rest);
@@ -72,15 +67,19 @@ char	*ft_get_next_line(int fd)
 			return (line_read);
 		}
 		buffer[bytes_read] = '\0';
-		line_read = prepare_line_read(buffer, rest);
+		line_read = prepare_line_read(buffer, rest); // TO DO Review Malloc with incorrect free
 		if (!line_read)
 			return (NULL);
 		nl_ptr = ft_strchr(line_read, '\n');
 		if (nl_ptr || bytes_read == 0)
 		{
 			nl_ptr[ft_strlen(nl_ptr)] = '\0';
-			rest = ft_strdup(nl_ptr + 1);
+			rest = ft_strdup(nl_ptr + 1); // TO DO Review Malloc with incorrect free
+			if (!rest)
+				freedom((void **)rest);
 			line_read = ft_substr(line_read, 0, nl_ptr - line_read);
+			if (!line_read)
+				freedom((void **)line_read);
 			freedom((void **)&buffer);
 			return (line_read);
 		}
